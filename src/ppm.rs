62fb5aad5e0f290f59921a0ad463070514c9c2d8
vismate <visnyarmate@gmail.com>
2022-12-26 21:40:03 +0100

    optimized ppm encoding by writing larger chunks to Writer

--- src/ppm.rs
+++ src/ppm.rs
@@ -20,9 +20,21 @@ pub fn encode_canvas(canvas: &Canvas, w: &mut dyn Write) -> Result<()> {
 pub fn encode_buffer(buffer: &[u32], width: usize, height: usize, w: &mut dyn Write) -> Result<()> {
     writeln!(w, "P6 {width} {height} 255")?;
 
-    for p in buffer {
-        let [b, g, r, _] = p.to_le_bytes();
-        w.write_all(&[r, g, b])?;
+    // instead of calling write on all pixels, we create chunks of 8192 bytes.
+    // this significantly increases performance even without the use of BufWriters.
+    // the number 8192 is taken from std::sys_common::io::DEFAULT_BUF_SIZE
+    // TODO: find a more reliable way of choosing a default chunk size (that also works well on other targets)
+    let mut data_chunks = buffer
+        .iter()
+        .flat_map(|p| p.to_be_bytes().into_iter().skip(1))
+        .array_chunks::<8192>();
+
+    for chunk in data_chunks.by_ref() {
+        w.write_all(chunk.as_slice())?;
+    }
+
+    if let Some(chunk) = data_chunks.into_remainder() {
+        w.write_all(chunk.as_slice())?;
     }
 
     Ok(())
